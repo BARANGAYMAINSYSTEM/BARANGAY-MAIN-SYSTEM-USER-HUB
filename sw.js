8d3f440aae42ad97475b8e27707836669fd15b78
BARANGAYMAINSYSTEM <seguirreadrianrey@gmail.com>
2026-04-08 21:34:03 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'brgy-hub-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+  // Add other assets here later, like '/icon-192.png', '/icon-512.png'
+];
+
+// Install Event: Caches the main files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch Event: Serves files from cache if offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate Event: Clears out old caches when updated
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
